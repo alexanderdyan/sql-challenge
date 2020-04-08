1561e9eb91964606587cfea88128b4da7ea7635b
alexanderdyan <58492089+alexanderdyan@users.noreply.github.com>
2020-04-07 21:54:41 -0700

    switched file content

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,55 +1,76 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS salaries;
-DROP TABLE IF EXISTS titles;
+-- List the following details of each employee: 
+-- employee number, last name, first name, gender, and salary.
 
-CREATE TABLE departments (
-	dept_no VARCHAR(30) NOT NULL,
-	dept_name VARCHAR(30) NOT NULL
-);
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no;
 
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR(20) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL
-);
+-- List employees who were hired in 1986
 
-CREATE TABLE dept_manager (
-	dept_no VARCHAR(5) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL
-);
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
 
-CREATE TABLE employees (
-	emp_no INT NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR(30) NOT NULL,
-	last_name VARCHAR(30) NOT NULL,
-	gender VARCHAR(1),
-	hire_date DATE NOT NULL
-);
+-- List the manager of each dept w/the following info:
+-- dept #, dept name, the manager's employee #, last name,
+-- first name, and start and end employment dates
 
-CREATE TABLE salaries (
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL
-);
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name, dept_manager.from_date, dept_manager.to_date
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
 
-CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR(30) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL
-);
+-- List the dept of each employee w/the following info: employee #, 
+-- last name, first name, and dept name
 
-SELECT * FROM departments;
-SELECT * FROM dept_emp;
-SELECT * FROM dept_manager;
-SELECT * FROM employees;
-SELECT * FROM salaries;
-SELECT * FROM titles;
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees 
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+
+-- List all employees whose first name is "Hercules" and last name
+-- begins with "B"
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+-- List all employees in the Sales dept, including their employee #,
+-- last name, and dept name
+
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+-- List all employees in the Sales and Development depts, including
+-- their employee #, last name, first name, and dept name
+
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales'
+OR departments.dept_name = 'Development';
+
+-- In descending order, list the frequency count of employee last names,
+-- i.e., how many employees share each last name
+
+SELECT last_name,
+COUNT(last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
+
